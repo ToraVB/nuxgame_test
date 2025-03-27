a6168750970969f340ff1edf4af2d8d156d95810
Viktor <d3nydb@gmail.com>
2025-03-27 14:00:41 +0200

    remove comments

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 set -e
 
-# Проверка и установка зависимостей Composer
 if [ ! -d "vendor" ]; then
     composer install --optimize-autoloader --no-interaction --no-dev
 fi
 
-# Генерация .env файла, если его нет
 if [ ! -f ".env" ]; then
     cp .env.example .env
     php artisan key:generate
